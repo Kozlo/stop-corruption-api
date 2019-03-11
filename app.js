118955dd35ae6a1d4ddcc937a5dd687d5c423e37
Martins Kozlovskis <mkozlovskis@gmail.com>
2019-03-11 20:35:42 +0200

    added most missing porperties (flat level checked only though...)

--- app.js
+++ app.js
@@ -60,7 +60,7 @@ app.use('/', routes);
 //=================
 
 // Fetch IUB data
-iubDataFetcher.fetchIUBData('2017', '01', '01');
+iubDataFetcher.fetchIUBData('2016', '12', '1');
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
